fa571ac371be6de25e295a33456a2fef40ee8f63
chibuikemokoli <chibuikemokoli@yahoo.com>
2023-07-31 00:50:27 +0100

    4005 4

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import "./NavbarStyles.css";
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+        <Link to="/">
+            <h1>Portfolio</h1>
+        </Link>
+        <ul className="nav-menu">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/Project">Project</Link>
+            </li>
+            <li>
+                <Link to="/About">About</Link>
+            </li>
+            <li>
+                <Link to="/Contact">Contact</Link>
+            </li>
+        </ul>
+        <div className="harmburger">
+            <FaBars size={20} style={{color:"#fff"}}/>
+        </div>
+    </div>
+  );
+};
+
+export default Navbar;
